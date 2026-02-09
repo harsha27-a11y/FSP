ec5811c356feb96a02b55697164143dfb323c35d
harsha27-a11y <harshavardhanmmurugesan@gmail.com>
2026-02-09 21:36:54 +0530

    Refactor server.js to improve route handling

--- server.js
+++ server.js
@@ -6,19 +6,26 @@ import todoRoutes from "./routes/todoRoutes.js";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/todos", todoRoutes);
-app.use("/api/active",(req,res)=>{
-  return res.status(200).json({
-    message:'This is new active route.'
-  })
-})
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+app.use("/api/todos", todoRoutes);
+
+app.use("/api/active", (req, res) => {
+  res.status(200).json({ message: "This is new active route." });
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
+  })
+  .catch(err => console.error(err));
